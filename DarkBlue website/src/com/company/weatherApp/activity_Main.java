3c1f0906ce55eb82ac8b19d063abb838ae7b1201
Unumberten <50941538+Unumberten@users.noreply.github.com>
2021-08-13 17:49:33 +0200

    Add files via upload

---
+++ DarkBlue website/src/com/company/weatherApp/activity_Main.java
@@ -0,0 +1,4 @@
+package com.company.weatherApp;
+
+public class activity_Main {
+}
---
+++ DarkBlue website/src/web/Weather-Teller/scripts/forecast.js
@@ -0,0 +1,26 @@
+
+const key = 'VJfjQ4tTvoSgmX32ALr8QSciVT1oxFbV';
+
+const getCity = async(city) =>{
+
+    const url = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(url+query);
+    const data = await response.json();
+    return data[0];
+
+}
+
+
+const getWeather = async(id) =>{
+
+    const url = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+    const response = await fetch(url+query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+
